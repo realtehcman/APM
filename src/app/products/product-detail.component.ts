5c76f6f5868519cca71176f2c98014e97c0a0c16
Max <max.dmytrenko@capgemini.com>
2022-10-21 14:28:01 +0200

    add product title to the view

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router  } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './IProduct';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-
 export class ProductDetailComponent implements OnInit {
   title = "Max' product detail";
   product: IProduct | undefined;
+  error = '';
 
-  constructor(private route: ActivatedRoute,
-              private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = Number(this.route.snapshot.paramMap.get('id'));
     this.title += `: ${id}`;
+    if (id) {
+      this.getProduct(id);
+    }
   }
 
   onBack(): void {
-      this.router.navigate(['/products']);
+    this.router.navigate(['/products']);
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: (pr) => (this.product = pr),
+      error: (err) => (this.error = err),
+    });
   }
 }
